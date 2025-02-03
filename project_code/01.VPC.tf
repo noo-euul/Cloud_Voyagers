6133efcc0d25f7ad3f8cbfd291d38f2f3936a30e
noo-euul <noeul1026@gmail.com>
2025-02-03 16:38:22 +0900

    01.VPC.tf

--- project_code/01.VPC.tf
+++ project_code/01.VPC.tf
@@ -1,27 +1,41 @@
-provider "aws" {
-  region = "ap-northeast-2" # 서울 리전
-}
-
-resource "aws_vpc" "seoul" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# 서울 VPC 서브넷
+resource "aws_subnet" "public_seoul" {
+  vpc_id                  = aws_vpc.seoul.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-2a"
   tags = {
-    Name = "CV_VPC_Seoul"
+    Name = "CV_Subnet_Public_Seoul"
   }
 }
 
-provider "aws" {
-  alias  = "france"
-  region = "eu-west-3" # 프랑스 리전
+resource "aws_subnet" "private_seoul" {
+  vpc_id            = aws_vpc.seoul.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "CV_Subnet_Private_Seoul"
+  }
 }
 
-resource "aws_vpc" "france" {
-  provider             = aws.france
-  cidr_block           = "10.1.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# 미국 버지니아 VPC 서브넷
+resource "aws_subnet" "public_Virginia" {
+  provider               = aws.Virginia
+  vpc_id                 = aws_vpc.Virginia.id
+  cidr_block             = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone      = "us-east-1a"
   tags = {
-    Name = "CV_VPC_France"
+    Name = "CV_Subnet_Public_Virginia"
+  }
+}
+
+resource "aws_subnet" "private_Virginia" {
+  provider         = aws.Virginia
+  vpc_id           = aws_vpc.Virginia.id
+  cidr_block       = "10.1.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "CV_Subnet_Private_Virginia"
   }
 }
