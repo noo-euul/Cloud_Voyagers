73fe2f9d5c820404e8be99a5fdc692ecec8298ac
noo-euul <noeul1026@gmail.com>
2025-03-06 10:16:44 +0900

    Update 02.Subnet.tf

--- project_code/02.Subnet.tf
+++ project_code/02.Subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "CV_public_seoul_a" {
   provider = aws.seoul
   vpc_id   = aws_vpc.CV_seoul_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.1.1.0/24"
   availability_zone = "ap-northeast-2a"
   map_public_ip_on_launch = true
 
@@ -14,7 +14,7 @@ resource "aws_subnet" "CV_public_seoul_a" {
 resource "aws_subnet" "CV_public_seoul_c" {
   provider = aws.seoul
   vpc_id   = aws_vpc.CV_seoul_vpc.id
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "10.1.3.0/24"
   availability_zone = "ap-northeast-2c"
   map_public_ip_on_launch = true
 
@@ -27,7 +27,7 @@ resource "aws_subnet" "CV_public_seoul_c" {
 resource "aws_subnet" "CV_private_seoul_a" {
   provider = aws.seoul
   vpc_id   = aws_vpc.CV_seoul_vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.1.2.0/24"
   availability_zone = "ap-northeast-2a"
 
   tags = {
@@ -38,7 +38,7 @@ resource "aws_subnet" "CV_private_seoul_a" {
 resource "aws_subnet" "CV_private_seoul_c" {
   provider = aws.seoul
   vpc_id   = aws_vpc.CV_seoul_vpc.id
-  cidr_block = "10.0.4.0/24"
+  cidr_block = "10.1.4.0/24"
   availability_zone = "ap-northeast-2c"
 
   tags = {
@@ -50,7 +50,7 @@ resource "aws_subnet" "CV_private_seoul_c" {
 resource "aws_subnet" "CV_public_virginia_a" {
   provider = aws.virginia
   vpc_id   = aws_vpc.CV_virginia_vpc.id
-  cidr_block = "10.1.1.0/24"
+  cidr_block = "10.2.1.0/24"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -62,7 +62,7 @@ resource "aws_subnet" "CV_public_virginia_a" {
 resource "aws_subnet" "CV_public_virginia_c" {
   provider = aws.virginia
   vpc_id   = aws_vpc.CV_virginia_vpc.id
-  cidr_block = "10.1.3.0/24"
+  cidr_block = "10.2.3.0/24"
   availability_zone = "us-east-1c"
   map_public_ip_on_launch = true
 
@@ -75,7 +75,7 @@ resource "aws_subnet" "CV_public_virginia_c" {
 resource "aws_subnet" "CV_private_virginia_a" {
   provider = aws.virginia
   vpc_id   = aws_vpc.CV_virginia_vpc.id
-  cidr_block = "10.1.2.0/24"
+  cidr_block = "10.2.2.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -86,7 +86,7 @@ resource "aws_subnet" "CV_private_virginia_a" {
 resource "aws_subnet" "CV_private_virginia_c" {
   provider = aws.virginia
   vpc_id   = aws_vpc.CV_virginia_vpc.id
-  cidr_block = "10.1.4.0/24"
+  cidr_block = "10.2.4.0/24"
   availability_zone = "us-east-1c"
 
   tags = {
