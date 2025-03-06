4bac2a2d9af6306210a1bf15fe79a651dfaac9ed
noo-euul <noeul1026@gmail.com>
2025-03-06 11:14:16 +0900

    Update 01.VPC.tf

--- project_code/01.VPC.tf
+++ project_code/01.VPC.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region = "ap-northeast-2"  # 서울 리전
+  alias  = "seoul"
+}
+
+provider "aws" {
+  region = "us-east-1"  # 버지니아 리전
+  alias  = "virginia"
+}
+
 resource "aws_vpc" "CV_seoul_vpc" {
   provider   = aws.seoul
   cidr_block = "10.1.0.0/16"
